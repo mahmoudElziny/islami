6cded1cf5e8ea59647acdb40817da67ba62a82fd
mahmoudElziny <mahmoudelziny20@gmail.com>
2024-02-25 10:46:07 +0200

    Finish Development 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:islami/app_theme.dart';
 import 'package:islami/home_screen.dart';
-import 'package:islami/tabs/hadeth/hadeth_details_screen.dart';
 import 'package:islami/tabs/quran/sura_details_screen.dart';
-import 'package:islami/tabs/settings/settings_provider.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 
 void main() {
   runApp(const IslamiApp());
@@ -18,7 +12,16 @@ class IslamiApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+        SuraDetailsScreen.routeName: (context) => SuraDetailsScreen(),
+      },
+      initialRoute: HomeScreen.routeName,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
     );
   }
 }
